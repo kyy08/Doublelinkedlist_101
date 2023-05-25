e37ae003a152ee0eaab51b5d35446c5c99ddb036
kyy08 <kyyelzz1@gmail.com>
2023-05-25 22:18:46 +0700

    DOubleLinkedList

--- Doublelinkedlist_101/Doublelinkedlist_101.cpp
+++ Doublelinkedlist_101/Doublelinkedlist_101.cpp
@@ -205,11 +205,13 @@ int main() {
 			default:
 				cout << "\nInvalid option" << endl;
 				break;
-
-
 			}
+		}
+		catch (exception& e) {
+			cout << "Check for the values entered." << endl;
 		}
 	}
 }
+
 
 
