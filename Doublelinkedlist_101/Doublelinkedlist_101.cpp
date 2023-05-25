7ca8b144bbab144c9a1b78f10b108c2d065e04d8
kyy08 <kyyelzz1@gmail.com>
2023-05-25 21:50:48 +0700

    void double LL::hapus

--- Doublelinkedlist_101/Doublelinkedlist_101.cpp
+++ Doublelinkedlist_101/Doublelinkedlist_101.cpp
@@ -136,4 +136,21 @@ void DoubleLinkedList::revtraverse() {
 	}
 }
 
+void  DoubleLinkedList::hapus() {
+	if (listEmpty()) {
+		cout << "\nlist is empty" << endl;
+
+	}
+	cout << "\nEnter the roll number of the student whose the record is to be deleted: ";
+	int rollNo;
+	cin >> rollNo;
+	cout << endl;
+	if (DoubleLinkedList::deleteNode(rollNo) == false)
+		cout << "Record not found" << endl;
+	else
+		cout << "Record with roll number" << rollNo << "deleted" << endl;
+}
+
+
+
 
