222e9a1e46ea1d4cfe74f032f4e9669727154cb7
kyy08 <kyyelzz1@gmail.com>
2023-05-25 22:09:22 +0700

    int main double LL

--- Doublelinkedlist_101/Doublelinkedlist_101.cpp
+++ Doublelinkedlist_101/Doublelinkedlist_101.cpp
@@ -169,4 +169,22 @@ void DoubleLinkedList::searchData() {
 	}
 }
 
+int main() {
+	DoubleLinkedList obj;
+	while (true) {
+		try {
+			cout << "\nMenu" << endl;
+			cout << "1. Add a record to the list" << endl;
+			cout << "2. Delete a record from the list" << endl;
+			cout << "3. View all records in the ascending order off roll number" << endl;
+			cout << "4. View all records in the descending order off roll number" << endl;
+			cout << " 5. Search for a record in the list" << endl;
+			cout << " 6. Exit" << endl;
+			cout << "\nEnter your choice (1-6): ";
+			char ch;
+			cin >> ch;
+		}
+	}
+}
+
 
