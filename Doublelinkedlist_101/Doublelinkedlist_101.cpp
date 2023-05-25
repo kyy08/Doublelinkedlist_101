3ffa686542dbb9e95b8323631271705f6c2ad6da
kyy08 <kyyelzz1@gmail.com>
2023-05-25 21:43:12 +0700

    else double LL:: revtraverse

--- Doublelinkedlist_101/Doublelinkedlist_101.cpp
+++ Doublelinkedlist_101/Doublelinkedlist_101.cpp
@@ -123,5 +123,17 @@ void DoubleLinkedList::revtraverse() {
 		cout << "\nlist is empty" << endl;
 	}
 
+	else {
+		cout << "\nRecords in descending order of roll number are:" << endl;
+		Node* currentNode = START;
+		while (currentNode->next != NULL)
+			currentNode = currentNode->next;
+
+		while (currentNode != NULL) {
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->prev;
+		}
+	}
+}
 
 
