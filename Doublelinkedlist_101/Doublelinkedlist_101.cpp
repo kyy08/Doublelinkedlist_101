2c820c3d8c5988b837d7baca5c87ac7be7e51034
kyy08 <kyyelzz1@gmail.com>
2023-05-25 21:19:41 +0700

    Double LL :: addNode

--- Doublelinkedlist_101/Doublelinkedlist_101.cpp
+++ Doublelinkedlist_101/Doublelinkedlist_101.cpp
@@ -29,6 +29,18 @@ DoubleLinkedList::DoubleLinkedList() {
 	START = NULL;
 }
 
+void DoubleLinkedList::addNode() {
+	int nim;
+	string nm;
+	cout << "\nEnter the roll number of the student: ";
+	cin >> nim;
+	cout << "\nEnter the name of the student: ";
+	cin >> nm;
+	Node* newNode = new Node(); //step 1
+	newNode->noMhs = nim; //step 2
+	newNode->name = nm; //step2
+
+
 
 
 	
